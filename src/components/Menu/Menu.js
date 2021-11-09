c6288b98032de6ec9172a2087896273975a0a00b
EKhoroshko <xapoxa14@gmail.com>
2021-11-10 01:28:30 +0200

    css menu

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -4,40 +4,40 @@ import css from '../Menu/Menu.module.css';
 import cn from 'classnames';
 
 const Menu = ({ isActive, handleClickBurgger }) => {
-    return (
-        <div className={cn(css.menuContainer, { [css.active]: isActive === true }, { [css.deactive]: isActive === false })}>
-            <div className={css.overlay} />
-            <div className={css.menuItems}>
-                <ul>
-                    <li>
-                        <Link to="/" onClick={() => handleClickBurgger()}>
-                            HOME
-                        </Link>
-                    </li>
-                    <li>
-                        <Link to="/game" onClick={() => handleClickBurgger()}>
-                            GAME
-                        </Link>
-                    </li>
-                    <li>
-                        <Link to="/about" onClick={() => handleClickBurgger()}>
-                            ABOUT
-                        </Link>
-                    </li>
-                    <li>
-                        <Link to="/contact" onClick={() => handleClickBurgger()}>
-                            CONTACT
-                        </Link>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    );
+  return (
+    <div className={cn(css.menuContainer, { [css.active]: isActive === true }, { [css.deactive]: isActive === false })}>
+      <div className={css.overlay} />
+      <div className={css.menuItems}>
+        <ul>
+          <li >
+            <Link className={css.item} to="/" onClick={() => handleClickBurgger()}>
+              <h4>HOME</h4>
+            </Link>
+          </li>
+          <li>
+            <Link className={css.item} to="/game" onClick={() => handleClickBurgger()}>
+              <h4>GAME</h4>
+            </Link>
+          </li>
+          <li>
+            <Link className={css.item} to="/about" onClick={() => handleClickBurgger()}>
+              <h4>ABOUT</h4>
+            </Link>
+          </li>
+          <li>
+            <Link className={css.item} to="/contact" onClick={() => handleClickBurgger()}>
+              <h4>CONTACT</h4>
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 Menu.propTypes = {
-    handleClickBurgger: PropTypes.func,
-    isActive: PropTypes.bool,
+  handleClickBurgger: PropTypes.func,
+  isActive: PropTypes.bool,
 };
 
 export default Menu;
