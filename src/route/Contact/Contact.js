785970c09829c688974bfdd419cc27b845983f68
EKhoroshko <xapoxa14@gmail.com>
2021-11-26 23:57:36 +0200

    contacts

--- src/route/Contact/Contact.js
+++ src/route/Contact/Contact.js
@@ -1,12 +1,19 @@
 import React from 'react';
-//import css from '../Contact/Contact.module.css'
+
+import css from '../Contact/Contact.module.css'
 
 const Contact = () => {
-    return (
-        <section >
-            <h1> Ведуться роботы</h1>
-        </section>
-    );
+  return (
+    <section className={css.wrapper}>
+      <div className={css.flex}>
+        <h1 className={css.title}> Our contacts:</h1>
+        <ul className={css.list}>
+          <li><span>Tel: </span><a href="tel:380930725966">+380930725966 </a></li>
+          <li><span>Email: </span><a href="mailto:xapoxa14@gmail.com"> write here </a></li>
+        </ul>
+      </div>
+    </section>
+  );
 }
 
 export default Contact;
